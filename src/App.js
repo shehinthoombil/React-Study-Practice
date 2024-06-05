9c56c553f91440af739c1d01246cc1f5cb48f959
Shehin Thoombil <shehinthoombil@gmail.com>
2024-06-05 20:01:28 +0530

    add Count

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
-import './App.css'
-import Header from './components/Header.js';
-function App() {
-  const data = "fortune kitchen" 
-  return (
-    <div>
-       <Header data ={data} />
-      <p> This is react js</p>
-    </div>
-  );
-}
+import { useState } from "react";
+import Counter from "./Counter.jsx";
 
+function App() {
+  const [count, setCount] = useState(0)
+  const addCount = () => {
+    setCount(count + 1)
+  }
+
+return (
+  <div>
+    <button onClick ={addCount}>click here</button>
+   <Counter count = {count} />
+  </div>
+);
+}
 export default App;
 
 
---
+++ src/Counter.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Counter(props) {
+  return (
+    <div>
+       <h1>counter : {props.count} </h1>
+    </div>
+  )
+}
+
+export default Counter
