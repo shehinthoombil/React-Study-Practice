a23062440c7da89ce6efceff31fad53a69aeb4a3
Shehin Thoombil <shehinthoombil@gmail.com>
2024-06-05 13:31:49 +0530

    functional component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,12 @@
-function Header(props){
-    return (<h1 className='hello'>HELLO SHEHIN'S {props.data} </h1>)
-    
-  }
+import { Component } from "react"
+
+class Header extends Component {
+    render(){
+        return (<h1 className='hello'>HELLO SHEHIN'S {this.props.data} </h1>)
+    }
+}
+
+// function Header(props){
+//     return (<h1 className='hello'>HELLO SHEHIN'S {props.data} </h1>)   
+//   }
   export default Header
