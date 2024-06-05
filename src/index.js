190ee01d980b09585cb6727f7cc6f7361b191f52
Shehin Thoombil <shehinthoombil@gmail.com>
2024-06-05 13:18:36 +0530

    create components and passing component another file using props method

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import App from './App.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -10,9 +8,6 @@ root.render(
     <App />
   </React.StrictMode>
 );
-// jiiiki
-// ijkik
-// If you want to start measuring performance in your app, pass a function uhuhuh
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+
+
