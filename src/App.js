0571083e18a5dc6e8d03b8fe7515811bc26f01b9
Shehin Thoombil <shehinthoombil@gmail.com>
2024-06-07 11:02:18 +0530

    useState, life cycle method

--- src/App.js
+++ src/App.js
@@ -1,29 +1,13 @@
+import Counter from "./Counter.js";
 import { useState } from "react";
-import Counter from "./Counter.jsx";
-import Employee from "./Employee.js";
-
 function App() {
-  const [count, setCount] = useState(0)
-  const addCount = () => {
-    setCount(count + 1)
-  }
-let emp = [{name:'shehin',age:22},
-{name:'adhil',age:24},
-{name:'thahir',age:27},
-]
-
-return (
-  <div>
-    <button onClick ={addCount}>click here</button>
-    {
-      emp.map((obj,index)=>{
-        return (
-          <Employee key = {index} {...obj}/>
-        )
-      })
-    }
-  </div>
-);
+  const [state, setState] = useState(false)
+  return (
+    <div className="App">
+      <h1 onClick={() => setState(!state)}>Hello shehin</h1>
+      {state && <Counter />}
+    </div>
+  )
 }
 export default App;
 
