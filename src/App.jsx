e99843c8ed57c218afefdb7de1ee5301a62e3190
Shehin Thoombil <shehinthoombil@gmail.com>
2024-06-20 16:56:41 +0530

    working of props

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,14 @@
-import Card from "./Card.jsx";
+import Button from "./Button.jsx";
+import Student from "./Student.jsx";
 
 function App() {
-   return (  
-    <>
-    <Card/>      
-    <Card/>      
-    <Card/>      
-    <Card/>      
-    <Card/>      
-    </>
-    
+   return ( 
+      <>
+      <Student name ="Shehin Thoombil" age = {22} isStudent = {true}/>
+      <Student name ="Dubai Jose" age = {35} isStudent = {false}/>
+      <Student name ="Dubai Jose" age = {35} isStudent = {false}/>
+      <Student name ="Dubai Jose" age = {35} isStudent = {false}/>
+      </> 
    )
 }
 export default App;
